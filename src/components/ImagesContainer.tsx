95e851c8878ea190a2d838945e9a558685ea0e4a
trybick <t.rybicki@gmail.com>
2022-05-16 19:00:52 -0400

    Add fade in animation

--- src/components/ImagesContainer.tsx
+++ src/components/ImagesContainer.tsx
@@ -1,10 +1,26 @@
 import { Flex, Image } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import imageSources from '../utils/images';
+
+const MotionImage = motion(Image);
+
+const motionProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  transition: { opacity: { delay: 0.3, duration: 3 } },
+};
 
 const ImagesContainer = () => (
   <Flex direction="column" align="center" justify="center" mt="30px">
     {imageSources.map((src) => (
-      <Image key={src} mb="30px" borderRadius="14px" src={src} width="300px" />
+      <MotionImage
+        key={src}
+        mb="30px"
+        borderRadius="14px"
+        src={src}
+        width="300px"
+        {...motionProps}
+      />
     ))}
   </Flex>
 );
