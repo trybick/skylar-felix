a16dbe104041edd67957edbfa7c2d826a7be9c76
trybick <t.rybicki@gmail.com>
2022-05-18 18:54:36 -0400

    Add custom cursor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,21 @@
+import { css, Global } from '@emotion/react';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 import { ISourceOptions } from 'tsparticles-engine';
 import particlesOptions from 'particles/particles.json';
+import cursorImage from 'images/cursor.png';
 import ImagesContainer from './ImagesContainer';
 import Footer from './Footer';
 
+const globalStyles = css`
+  * {
+    cursor: url(${cursorImage}), auto;
+  }
+`;
+
 const App = () => (
   <>
+    <Global styles={globalStyles} />
     <Particles init={loadFull} options={particlesOptions as ISourceOptions} />
     <ImagesContainer />
     <Footer />
