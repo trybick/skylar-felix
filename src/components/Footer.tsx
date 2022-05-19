1bcdf13dbe4d3af80725b487691a7adfed04648e
trybick <t.rybicki@gmail.com>
2022-05-19 18:39:16 -0400

    Add filter drop shadow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Flex, Icon, Link } from '@chakra-ui/react';
 import { VscGithub } from 'react-icons/vsc';
 
 const Footer = () => (
-  <Flex justify="center" m="10px 0 30px">
+  <Flex justify="center" m="10px 0 20px">
     <Link href="https://github.com/trybick/skylar-life" isExternal>
       <Icon as={VscGithub} boxSize="22px" color="skyblue" />
     </Link>
--- src/components/ImagesContainer.tsx
+++ src/components/ImagesContainer.tsx
@@ -7,18 +7,20 @@ const MotionImage = motion(Image);
 const motionProps = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
-  transition: { opacity: { delay: 0.3, duration: 3 } },
+  transition: { opacity: { delay: 0.5, duration: 2 } },
 };
 
 const ImagesContainer = () => (
   <Flex align="center" direction="column" justify="center" mt="30px">
     {imageSources.map((src) => (
       <MotionImage
-        borderRadius="14px"
+        borderRadius="20px"
         key={src}
+        maxW="85%"
         mb="30px"
         src={src}
-        width="300px"
+        width="400px"
+        _hover={{ filter: 'drop-shadow(0 0 1.3rem deepskyblue)' }}
         {...motionProps}
       />
     ))}
