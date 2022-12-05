827598251e13aec33c8875b5d94d6931a7d741b8
trybick <t.rybicki@gmail.com>
2022-12-04 19:16:46 -0500

    Base

--- src/components/ImagesContainer.tsx
+++ src/components/ImagesContainer.tsx
@@ -1,6 +1,6 @@
 import { Flex, Image } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-import imageSources from 'utils/images';
+import { imageSources } from 'utils/images';
 
 const MotionImage = motion(Image);
 
@@ -12,15 +12,15 @@ const motionProps = {
 
 const ImagesContainer = () => (
   <Flex align="center" direction="column" justify="center" mt="30px">
-    {imageSources.map((src) => (
+    {imageSources.map((src: any) => (
       <MotionImage
+        _hover={{ filter: 'drop-shadow(0 0 1.3rem deepskyblue)' }}
         borderRadius="20px"
         key={src}
         maxW="85%"
-        mb="30px"
+        mb="40px"
         src={src}
         width="400px"
-        _hover={{ filter: 'drop-shadow(0 0 1.3rem deepskyblue)' }}
         {...motionProps}
       />
     ))}
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,8 +1,9 @@
-import skylar01 from 'images/skylar01.jpg';
-import skylar02 from 'images/skylar02.jpg';
-import skylar03 from 'images/skylar03.jpg';
-import skylar04 from 'images/skylar04.jpg';
+const importAll = (context: any) =>
+  context
+    .keys()
+    .filter((key: string) => !key.includes('./')) // remove duplicates
+    .map(context)
+    .reverse();
 
-const images = [skylar01, skylar02, skylar03, skylar04];
-
-export default images.slice(0).reverse();
+// @ts-ignore
+export const imageSources = importAll(require.context('../images', false, /\.(jpg)$/));
