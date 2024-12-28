d8bcb420c90f4bf996b1d13944cd9573276c0923
trybick <t.rybicki@gmail.com>
2024-12-28 10:23:17 -0500

    Remove motion image delay transition

--- src/components/ImagesContainer.tsx
+++ src/components/ImagesContainer.tsx
@@ -1,19 +1,10 @@
 import { Flex, Image } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
 import { imageSources } from 'utils/images';
-
-const MotionImage = motion(Image);
-
-const motionProps = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  transition: { opacity: { delay: 0.5, duration: 2 } },
-};
 
 const ImagesContainer = () => (
   <Flex align="center" direction="column" justify="center" mt="30px">
     {imageSources.map((src) => (
-      <MotionImage
+      <Image
         _hover={{ filter: 'drop-shadow(0 0 1.3rem deepskyblue)' }}
         borderRadius="20px"
         key={src}
@@ -21,7 +12,6 @@ const ImagesContainer = () => (
         mb="40px"
         src={src}
         width="400px"
-        {...motionProps}
       />
     ))}
   </Flex>
