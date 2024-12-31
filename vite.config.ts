9d5e3be871b3495da7f0a92ecd41096faeab2743
trybick <t.rybicki@gmail.com>
2024-12-31 11:13:59 -0500

    Remove env define in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_SITE_NAME': JSON.stringify(process.env.VITE_SITE_NAME),
-  },
   server: {
     open: true,
     port: 4000,
