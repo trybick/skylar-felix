83a87260418cfab0b961ee1bd118538c482c4081
trybick <t.rybicki@gmail.com>
2024-12-31 14:29:08 -0500

    Big update on particles

--- src/components/ParticlesContainer.tsx
+++ src/components/ParticlesContainer.tsx
@@ -1,10 +1,30 @@
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
-import { ISourceOptions } from 'tsparticles-engine';
-import particlesOptions from '../particles.json';
+import { useEffect, useState } from 'react';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
+import { particleOptions } from '../particleOptions';
 
-const ParticlesContainer = () => (
-  <Particles init={loadFull} options={particlesOptions as ISourceOptions} />
-);
+const ParticlesContainer = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const getRandomParticles = () => {
+    const keys = Object.keys(particleOptions);
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    return particleOptions[keys[randomIndex]];
+  };
+
+  return init ? (
+    <div style={{ position: 'absolute', zIndex: -1 }}>
+      <Particles options={getRandomParticles()} />
+    </div>
+  ) : null;
+};
 
 export default ParticlesContainer;
---
+++ src/particleOptions.ts
@@ -0,0 +1,290 @@
+import { ISourceOptions } from '@tsparticles/engine';
+
+// Templates from:
+// https://github.com/tsparticles/tsparticles/tree/main/utils/configs/src
+
+export const particleOptions: Record<string, ISourceOptions> = {
+  // -------------- Pop --------------
+  pop: {
+    particles: {
+      destroy: {
+        mode: 'split',
+        split: {
+          count: 1,
+          factor: {
+            value: {
+              min: 2,
+              max: 4,
+            },
+          },
+          rate: {
+            value: 100,
+          },
+          particles: {
+            life: {
+              count: 1,
+              duration: {
+                value: {
+                  min: 2,
+                  max: 3,
+                },
+              },
+            },
+            move: {
+              speed: {
+                min: 10,
+                max: 15,
+              },
+            },
+          },
+        },
+      },
+      number: {
+        value: 80,
+      },
+      color: {
+        value: [
+          '#3998D0',
+          '#2EB6AF',
+          '#A9BD33',
+          '#FEC73B',
+          '#F89930',
+          '#F45623',
+          '#D62E32',
+          '#EB586E',
+          '#9952CF',
+        ],
+      },
+      shape: {
+        type: 'circle',
+      },
+      opacity: {
+        value: 1,
+      },
+      size: {
+        value: {
+          min: 10,
+          max: 15,
+        },
+      },
+      collisions: {
+        enable: true,
+        mode: 'bounce',
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        outModes: 'bounce',
+      },
+    },
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: 'pop',
+        },
+      },
+    },
+    background: {
+      color: '#000000',
+    },
+  },
+  // -------------- Absorber --------------
+  absorber: {
+    key: 'absorbers',
+    name: 'Absorbers',
+    particles: {
+      number: {
+        value: 300,
+      },
+      collisions: {
+        enable: true,
+      },
+      color: {
+        value: '#ffffff',
+      },
+      shape: {
+        type: 'circle',
+      },
+      opacity: {
+        value: {
+          min: 0.1,
+          max: 1,
+        },
+      },
+      size: {
+        value: {
+          min: 1,
+          max: 2,
+        },
+      },
+      move: {
+        enable: true,
+        speed: 0.5,
+        direction: 'top',
+        straight: true,
+        warp: true,
+      },
+    },
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: 'push',
+        },
+      },
+      modes: {
+        push: {
+          quantity: 10,
+        },
+      },
+    },
+    absorbers: {
+      draggable: true,
+      size: {
+        value: {
+          min: 5,
+          max: 10,
+        },
+        limit: 10,
+      },
+      position: {
+        x: 50,
+        y: 50,
+      },
+    },
+    background: {
+      color: '#0d47a1',
+    },
+  },
+  // -------------- Snow --------------
+  snow: {
+    key: 'snow',
+    name: 'Snow',
+    particles: {
+      number: {
+        value: 400,
+        density: {
+          enable: true,
+        },
+      },
+      color: {
+        value: '#fff',
+      },
+      shape: {
+        type: 'circle',
+      },
+      opacity: {
+        value: 1,
+      },
+      size: {
+        value: 10,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: 'bottom',
+        straight: true,
+      },
+      wobble: {
+        enable: true,
+        distance: 10,
+        speed: 10,
+      },
+      zIndex: {
+        value: {
+          min: 0,
+          max: 100,
+        },
+        opacityRate: 10,
+        sizeRate: 10,
+        velocityRate: 10,
+      },
+    },
+    background: {
+      color: '#333333',
+    },
+  },
+  // -------------- Nasa --------------
+  nasa: {
+    key: 'nasa',
+    name: 'NASA',
+    particles: {
+      number: {
+        value: 160,
+        density: {
+          enable: true,
+        },
+      },
+      color: {
+        value: '#ffffff',
+      },
+      shape: {
+        type: 'circle',
+      },
+      opacity: {
+        value: {
+          min: 0.1,
+          max: 1,
+        },
+        animation: {
+          enable: true,
+          speed: 1,
+          sync: false,
+        },
+      },
+      size: {
+        value: {
+          min: 1,
+          max: 3,
+        },
+      },
+      move: {
+        enable: true,
+        speed: {
+          min: 0.1,
+          max: 1,
+        },
+      },
+    },
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: 'bubble',
+        },
+        onClick: {
+          enable: true,
+          mode: 'repulse',
+        },
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          links: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 250,
+          size: 0,
+          duration: 2,
+          opacity: 0,
+        },
+        repulse: {
+          distance: 400,
+          duration: 0.4,
+        },
+        push: {
+          quantity: 4,
+        },
+        remove: {
+          quantity: 2,
+        },
+      },
+    },
+    background: {
+      color: '#232741',
+    },
+  },
+};
